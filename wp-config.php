f1efde95d3d70c842dc273ee597e0700158ca366
Vasudevan <vasudevan.r@krossark.com>
2025-07-29 11:26:04 +0530

    update for  force https redirect

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The base configuration for WordPress
  *
@@ -21,22 +22,22 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'railway' );
+define('DB_NAME', 'railway');
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define('DB_USER', 'root');
 
 /** Database password */
-define( 'DB_PASSWORD', 'UuSIrUptmgXqUBPIeJoTNEYeerUbLUtg' );
+define('DB_PASSWORD', 'UuSIrUptmgXqUBPIeJoTNEYeerUbLUtg');
 
 /** Database hostname (include port) */
-define( 'DB_HOST', 'shortline.proxy.rlwy.net:11086' );
+define('DB_HOST', 'shortline.proxy.rlwy.net:11086');
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define('DB_CHARSET', 'utf8mb4');
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication unique keys and salts.
@@ -49,14 +50,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '#G1u->l}#8(!AiT_g+kC4J00Vf3mkAnmdYOU*:zK;X>k(O3#eDpZlGIE{/Bqvghu' );
-define( 'SECURE_AUTH_KEY',  'C!5PJjiw:tv]M lj}=S>B)=`ELjD3L8$NZ=Xe/>62|T38;p=C3OFKp;jU`b+hk[W' );
-define( 'LOGGED_IN_KEY',    '~K43%Ec6*u8xy3a-qOSS?AscGg5c9Cfr9u^IEu.#BRmob5i[uU3?#,K>2OARXUE1' );
-define( 'NONCE_KEY',        ' skMkzVF^zkIUMye*V6cG|1TB*vc}4&={H Dvvu;Kp/]n1V@I*B0bUiFnq.Y0#$E' );
-define( 'AUTH_SALT',        '(Y}K*mYV,}r;4|9V`9r);UKt%.7PlA:lan<>|4V?Gje&S;T%doXSkbKc2=UD nd^' );
-define( 'SECURE_AUTH_SALT', 'w#jA<G[_>9Sqt//)kQVXqy{_?Yb9)%u{FZ+nNG-5TM}.o{@jZr=:1w$9XFqa~AGw' );
-define( 'LOGGED_IN_SALT',   '2asS<;yQ9#8]d<Y~oqB{0Wh:[s$K]a>eDwiIBK&tjy{$4Gs{q1>K/8#$cE^+HgfL' );
-define( 'NONCE_SALT',       'mAgpBMW.|w+q. Vc]%aE)8V7`E#<<KBB%r1vKZ6VBZ&*k.2y$!xW6dPudqftPdjh' );
+define('AUTH_KEY',         '#G1u->l}#8(!AiT_g+kC4J00Vf3mkAnmdYOU*:zK;X>k(O3#eDpZlGIE{/Bqvghu');
+define('SECURE_AUTH_KEY',  'C!5PJjiw:tv]M lj}=S>B)=`ELjD3L8$NZ=Xe/>62|T38;p=C3OFKp;jU`b+hk[W');
+define('LOGGED_IN_KEY',    '~K43%Ec6*u8xy3a-qOSS?AscGg5c9Cfr9u^IEu.#BRmob5i[uU3?#,K>2OARXUE1');
+define('NONCE_KEY',        ' skMkzVF^zkIUMye*V6cG|1TB*vc}4&={H Dvvu;Kp/]n1V@I*B0bUiFnq.Y0#$E');
+define('AUTH_SALT',        '(Y}K*mYV,}r;4|9V`9r);UKt%.7PlA:lan<>|4V?Gje&S;T%doXSkbKc2=UD nd^');
+define('SECURE_AUTH_SALT', 'w#jA<G[_>9Sqt//)kQVXqy{_?Yb9)%u{FZ+nNG-5TM}.o{@jZr=:1w$9XFqa~AGw');
+define('LOGGED_IN_SALT',   '2asS<;yQ9#8]d<Y~oqB{0Wh:[s$K]a>eDwiIBK&tjy{$4Gs{q1>K/8#$cE^+HgfL');
+define('NONCE_SALT',       'mAgpBMW.|w+q. Vc]%aE)8V7`E#<<KBB%r1vKZ6VBZ&*k.2y$!xW6dPudqftPdjh');
 
 /**#@-*/
 
@@ -86,7 +87,7 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', false);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
@@ -95,9 +96,13 @@ define( 'WP_DEBUG', false );
 /* That's all, stop editing! Happy publishing. */
 define('WP_HOME', 'https://lapp-production.up.railway.app');
 define('WP_SITEURL', 'https://lapp-production.up.railway.app');
+// Force WordPress to recognize HTTPS behind Railway proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (! defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
