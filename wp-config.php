8e5b96cc59098a5afc277dd232beabdb7df35803
manoj <69884509+manojkrossark@users.noreply.github.com>
2025-08-14 11:08:15 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -28,10 +28,10 @@ define('DB_NAME', 'railway');
 define('DB_USER', 'root');
 
 /** Database password */
-define('DB_PASSWORD', 'UuSIrUptmgXqUBPIeJoTNEYeerUbLUtg');
+define('DB_PASSWORD', 'PhWbqymObTVfujpubVbAuoqwYkruBxui');
 
 /** Database hostname (include port) */
-define('DB_HOST', 'shortline.proxy.rlwy.net:11086');
+define('DB_HOST', 'ballast.proxy.rlwy.net:28065');
 
 /** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
