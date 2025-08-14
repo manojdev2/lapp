2888b5867070c183ab7145ee963dbfd0a79fcd97
kalaiyarasan-p1 <kalaiyarasan.p@krossark.com>
2025-08-14 22:52:20 +0530

    <kalaiyarasan> function php correction

--- wp-content/themes/igual-child/functions.php
+++ wp-content/themes/igual-child/functions.php
@@ -3,8 +3,6 @@
  * Enqueues parent theme stylesheet
  * ========================================= */
 
-add_action( 'wp_enqueue_scripts', 'igual_enqueue_child_theme_styles', 30 );
-function igual_enqueue_child_theme_styles() {
 add_action('wp_enqueue_scripts', 'igual_enqueue_child_theme_styles', 30);
 function igual_enqueue_child_theme_styles()
 {
